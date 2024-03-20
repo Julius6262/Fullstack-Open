3e09a508780f92340a1a7b35fbe15c5c156bcd5c
Martin <jo.ju.martin@gmail.com>
2024-03-20 17:31:13 +0100

    1.12*: anecdotes step1 exercise solution

--- 1.6 -1.14 unicafe/unicafe/src/App.jsx
+++ 1.6 -1.14 unicafe/unicafe/src/App.jsx
@@ -12,14 +12,39 @@ const StatisticLine = ({ label, value }) => (
   </p>
 );
 
+// Pick randon anecdote
+const RandomAnecdotes = ({anecdotes, selected}) => {
+  return(
+    <div>
+      <p>{anecdotes[selected]}</p>
+    </div>
+  );
+};
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
   const [goodCount, setGoodCount] = useState(0);
   const [neutralCount, setNeutralCount] = useState(0);
   const [badCount, setBadCount] = useState(0);
+  const [selected, setSelected] = useState(0);
 
   const handleGoodClick = () => setGoodCount(goodCount + 1);
   const handleNeutralClick = () => setNeutralCount(neutralCount + 1);
   const handleBadClick = () => setBadCount(badCount + 1);
+  const handleSelectedClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+    console.log("This is randomindex" + Math.floor(Math.random() * (anecdotes.length + 1)))  // we round down, so to include the full lenght of the array we add 1
+  };
 
   const total = goodCount + neutralCount + badCount;
   const average = (goodCount * 1 + neutralCount * 0 + badCount * -1) / total;
@@ -31,7 +56,10 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="Good" />
       <Button handleClick={handleNeutralClick} text="Neutral" />
       <Button handleClick={handleBadClick} text="Bad" />
-
+      <br></br>
+      <br></br>
+      <Button handleClick={handleSelectedClick} text="next anecdote" />
+      <RandomAnecdotes anecdotes={anecdotes} selected={selected}/>
       <h2>Statistics</h2>
     {total > 0 ? (
       <table>
